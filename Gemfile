22bc97f903aa6a75712431c076657eeab27cf210
Julian Bäume <julian@svg4all.de>
2011-07-17 23:49:18 +0200

    initial commit
    creating a rails app for the app

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.1.beta'
+
+gem 'sqlite3'
+
+# Asset template engines
+gem 'json'
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'therubyracer'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anduin::Application.config.secret_token = '7ce9e1c9ce8cba752a8684e7964d967aafe50ee800a8ffc28d18020733db46358318400a2631b2b6b470525cf159eadb461ccb8371fdbedbeedb4a8c7fe16840'
