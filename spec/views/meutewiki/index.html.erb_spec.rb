7b2a1b790f777577401253d964c43b5babce336b
Julian Bäume <julian@svg4all.de>
2011-07-19 20:58:12 +0200

    implement index view

--- spec/views/meutewiki/index.html.erb_spec.rb
+++ spec/views/meutewiki/index.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe "meutewiki/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should render a list of pages" do
+    assign(:wiki_pages,
+           [double("Gollum::Page", :name => 'TestPage1'),
+            double("Gollum::Page", :name => 'TestPage2')]
+          )
+    render
+    rendered.should have_css("a", :href => meutewiki_show_page_path('TestPage1'))
+    rendered.should have_css("a", :href => meutewiki_show_page_path('TestPage2'))
+  end
 end
