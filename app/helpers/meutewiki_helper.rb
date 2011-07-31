a5a1e8a7498914cd902152158b3ca8fb245f8ad4
Julian Bäume <julian@svg4all.de>
2011-07-31 17:40:56 +0200

    add sidebar menu for meutewiki page show action
    for now, there is only the edit link working, history link is provided
    as an example

--- app/helpers/meutewiki_helper.rb
+++ app/helpers/meutewiki_helper.rb
@@ -1,2 +1,10 @@
 module MeutewikiHelper
+  def menu_head(title)
+    Haml::Engine.new("%h3.menu #{title}").render
+  end
+
+  def menu_item(name, url)
+    l = link_to name, url
+    Haml::Engine.new("%li.menu #{l}").render
+  end
 end
