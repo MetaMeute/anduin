e9799899059e9da29d433c30484d351cab7bb6d4
Julian Bäume <julian@svg4all.de>
2011-07-31 16:35:11 +0200

    add a small hack to provide internationalisation
    this can be done more clean, but it works in most cases to provide a
    simple internationalisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  protected
+  def set_locale
+    available = %w{en de}
+    locale = available.select { request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first } || "en"
+    I18n.locale = locale
+  end
 end
