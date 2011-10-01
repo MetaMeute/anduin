2e7828c7043374c07a1cf9d922a1cfeba07563d2
Julian Bäume <julian@svg4all.de>
2011-10-01 20:51:32 +0200

    remove some devise modules
     * don’t allow people to register themselfes. Need to find a good solution
       for that (accounts need to be created in ldap)
     * don’t allow people to reset their passwords, since we need ldap-admin
       rights for that

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :ldap_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :ldap_authenticatable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
