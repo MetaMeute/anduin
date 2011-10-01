26529087d6e54b17fc6649827df3ebbdb5f78540
Julian Bäume <julian@svg4all.de>
2011-10-01 22:45:59 +0200

    set links in navigation to decent values

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,8 +1,8 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
-    primary.item :root, t(:front_page, :scope => :menu), root_path
-    primary.item :meute, t(:MetaMeute, :scope => :menu), root_path
-    primary.item :projects, t(:projects, :scope => :menu), root_path
+    primary.item :root, t(:front_page, :scope => :menu), meutewiki_show_page_path("FrontPage")
+    primary.item :meute, t(:MetaMeute, :scope => :menu), meutewiki_show_page_path("MetaMeute")
+    primary.item :projects, t(:projects, :scope => :menu), meutewiki_show_page_path("MetaProjects")
     primary.item :login, t(:login), new_user_session_path, :class => 'right', :unless => Proc.new { user_signed_in? }
     primary.item :logout, t(:logout), destroy_user_session_path, :class => 'right', :if => Proc.new { user_signed_in? }
   end
