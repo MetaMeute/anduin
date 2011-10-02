94314636e00fadeb418407f4758c23ab1feaa430
Julian Bäume <julian@svg4all.de>
2011-10-02 03:11:01 +0200

    remove obsolete specs from user_spec
    since we authenticate against ldap, now. we don’t need to store the
    password any longer. also the e-mail is not the primary key for users,
    but nick is. uniqueness of the nick is made sure by the ldap.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,15 @@ require 'spec_helper'
 
 describe User do
   describe "minimal information" do
-    it "should be valid with email and password" do
+    it "should be valid with email" do
       u = User.new(:email => 'test@example.com')
-      u.password='testpassword'
       u.should be_valid
-    end
-
-    describe "should not be valid" do
-      it { User.new(:email => 'test@example.com').should_not be_valid }
-      it { User.new(:email => 'wrongmail', :password => 'testpassword').should_not be_valid }
     end
   end
 
   describe "optional information" do
     let(:user) { User.find_by_email('test@example.com') }
-    before(:each) { User.create!(:email => 'test@example.com', :password => 'testpassword') }
+    before(:each) { User.create!(:email => 'test@example.com') }
 
     after(:each) { user.should be_valid }
     it "might have a nickname" do
