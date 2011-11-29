8d795e58259a00e4f857ea18de904cafddc28695
Julian Bäume <julian@svg4all.de>
2011-11-29 03:30:08 +0100

    fix users controller specs
    The HTTP-header needs to contain a language, or else the controller will
    crash.
    Also we now override the views of devise with our own

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe UsersController do
+  before(:each) do
+    request.env['HTTP_ACCEPT_LANGUAGE'] = "de, en"
+  end
 
   describe "GET 'sign_up'" do
     it "should be successful" do
       get 'sign_up'
-      response.should render_template('devise/registrations/new')
+      response.should render_template('users/sign_up', 'layout/application')
     end
   end
 
