5e0277eaf133aa06a2ad8a50661f71b1b3c771ce
Julian Bäume <julian@svg4all.de>
2012-03-06 16:12:51 +0100

    add step definition: test for uploaded wiki-files
    When a user uploads a file for the wiki, it should be recognized as
    such. This adds a (failing) step definition to test this behaviour.
    
    I’m not sure, why this is failing at all. Needs investigation.

--- features/step_definitions/wiki_steps.rb
+++ features/step_definitions/wiki_steps.rb
@@ -2,3 +2,9 @@ Then /^I should be on a wiki page named "([^"]*)"$/ do |page_name|
   current_path.should == meutewiki_show_page_path(page_name)
 end
 
+Then /^the file "([^"]*)" is part of the wiki$/ do |file_name|
+  file = FileAsset.find_by_file_file_name(:file_name)
+  file.should_not be_nil
+  file.asset.classifications.find( Catalog.find_by_name(:wiki_files) ).should be_true
+end
+
