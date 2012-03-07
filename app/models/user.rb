21fbb2325b7f5cbc0e7d3ef9dbb7c5937a7e1301
Julian Bäume <julian@svg4all.de>
2012-03-07 17:01:04 +0100

    add password recover links
    Activate forgot password links, makes less tests fail :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :ldap_authenticatable, :rememberable, :trackable, :registerable
+  devise :ldap_authenticatable, :rememberable, :trackable, :registerable, :recoverable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :nick, :email, :password, :password_confirmation, :remember_me
